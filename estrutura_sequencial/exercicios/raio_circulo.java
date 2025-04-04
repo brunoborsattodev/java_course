e0e88a35d8ad058a892e7b84b18bef9d6a8bc831
guerumi <bruno.borsatto.costa@gmail.com>
2025-04-04 17:04:21 -0300

    pasta Estrutura Sequencial

--- estrutura_sequencial/exercicios/raio_circulo.java
+++ estrutura_sequencial/exercicios/raio_circulo.java
@@ -1,4 +1,4 @@
-package exercicios;
+package estrutura_sequencial.exercicios;
 
 //Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
 //        casas decimais conforme exemplos.
--- estrutura_sequencial/exercicios/valor_a_pagar.java
+++ estrutura_sequencial/exercicios/valor_a_pagar.java
@@ -1,4 +1,4 @@
-package exercicios;
+package estrutura_sequencial.exercicios;
 
 import java.util.Scanner;
 
