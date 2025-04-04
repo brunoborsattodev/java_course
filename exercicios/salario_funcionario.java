99fc01c69cff4ac5c38279153d851512c869eb08
guerumi <bruno.borsatto.costa@gmail.com>
2025-04-04 16:22:00 -0300

    calculo de salario funcionarios

---
+++ exercicios/salario_funcionario.java
@@ -0,0 +1,23 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class salario_funcionario {
+    public static void main(String[] args) {
+
+//        Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por
+//        hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas
+//        decimais.
+
+        Scanner sc = new Scanner(System.in);
+        double num, hrs, valorHr, salario;
+
+
+        num = sc.nextDouble();
+        hrs = sc.nextDouble();
+        valorHr = sc.nextDouble();
+        salario = hrs * valorHr;
+        System.out.println("NUMBER = " + num);
+        System.out.println("SALARY = U$ " + salario);
+    }
+}
