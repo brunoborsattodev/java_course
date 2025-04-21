4d6767c2754cceeb38416c8f009f393fa1e6e5d8
guerumi <bruno.borsatto.costa@gmail.com>
2025-04-21 12:03:09 -0300

    membros estáticos

---
+++ POO/aula.java
@@ -0,0 +1,32 @@
+package POO;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import POO.util.Calculator;
+
+public class aula {
+
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        Calculator calc = new Calculator();
+
+        System.out.print("Enter radius: ");
+        double radius = sc.nextDouble();
+
+        double c = calc.circumference(radius);
+
+        double v = calc.volume(radius);
+
+        System.out.printf("Circumference: %.2f\n", c);
+        System.out.printf("Volume: %.2f\n", v);
+        System.out.printf("PI Value: %.2f\n", calc.PI);
+
+        sc.close();
+    }
+
+}
