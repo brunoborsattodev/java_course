55a06e779c231c0a8bb9dbc54ea31ec6d26695aa
guerumi <bruno.borsatto.costa@gmail.com>
2025-04-20 22:13:43 -0300

    exercício funcionario POO

---
+++ POO/entities/Employee.java
@@ -0,0 +1,24 @@
+package POO.entities;
+
+public class Employee {
+    public String name;
+    public double grossSalary;
+    public double tax;
+
+    public double NetSalary(){
+        return grossSalary - tax;
+    }
+
+    public void IncreaseSalary(double percentage){
+        double increase = grossSalary * (percentage / 100);
+        this.grossSalary += increase;
+    }
+
+    public String toString(){
+        return name
+                + ", "
+                + " $ "
+                + NetSalary();
+    }
+
+}
---
+++ POO/ex/funcionario.java
@@ -0,0 +1,36 @@
+package POO.ex;
+
+import POO.entities.Employee;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class funcionario {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        Employee employee = new Employee();
+
+        System.out.println("Name = ");
+        employee.name = sc.nextLine();
+
+        System.out.println("Gross salary = ");
+        employee.grossSalary = sc.nextDouble();
+
+        System.out.println("Tax = ");
+        employee.tax = sc.nextDouble();
+
+        System.out.println(employee);
+
+        System.out.println("Which percentage to increase salary? = ");
+        double percentage = sc.nextDouble();
+        employee.IncreaseSalary(percentage);
+
+        System.out.print("Updated data = ");
+        System.out.println(employee);
+
+        sc.close();
+    }
+}
