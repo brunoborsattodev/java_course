fbb87cc6e9bc7a84fe0f738faee52e43f8da332b
guerumi <bruno.borsatto.costa@gmail.com>
2025-04-21 13:38:18 -0300

    aprendendo Construtores e palavra this

---
+++ POO/construtores/produtoEmEstoque2.java
@@ -0,0 +1,43 @@
+package POO.construtores;
+
+import POO.entities.Product2;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class produtoEmEstoque2 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter product data: ");
+
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Price:");
+        double price = sc.nextDouble();
+
+        System.out.print("Quantity in stock: ");
+        int quantity = sc.nextInt();
+
+        Product2 product = new Product2(name, price, quantity);
+
+        System.out.println("\nProduct data: " + product);
+
+        System.out.println("Enter the number of products to be added in stock = ");
+        quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println("\nUpdated data: " + product);
+
+        System.out.println("Enter the number of products to be removed from stock = ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println("\nUpdated data: " + product);
+
+        sc.close();
+    }
+}
