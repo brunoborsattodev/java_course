b8aac353008b781f5ebfd0aa011f1312de9176dd
guerumi <bruno.borsatto.costa@gmail.com>
2025-05-21 23:22:14 -0300

    POO BUILD & RUN: Máquina de Café

--- POO/abstracaoEncapsulamento/cafeteria/DolceGusto.java
+++ POO/abstracaoEncapsulamento/cafeteria/DolceGusto.java
@@ -10,53 +10,93 @@ public class DolceGusto implements MaquinaCafe {
 
     @Override
     public void ligar() {
+        this.ligada = true;
         System.out.println("LIGANDO CAFETEIRA...");
         // ligar cafeteira
     }
 
     @Override
+    public void desligar() {
+        this.ligada = false;
+        System.out.println("DESLIGANDO CAFETEIRA...");
+
+        //desliga a maquina de café
+    }
+
+    @Override
     public void colocarCachimbo() {
+        this.possuiCachimbo = true;
         System.out.println("CACHIMBO INSERIDO");
         //colocar o cachimbo na cafeteira
     }
 
     @Override
     public void retirarCachimbo() {
+        this.possuiCachimbo = false;
         System.out.println("CACHIMBO RETIRADO");
         //retirar o cachimbo na cafeteira
     }
 
     @Override
     public void selecionarModo(ModoCafe modoCafe) {
-        System.out.println("Modo de Café Selecionado: " + ModoCafe.getDescricao());
+        System.out.println("Modo de Café Selecionado: " + modoCafe.getDescricao());
+        this.modoCafeSelecionado = modoCafe;
         //seleciona o modo do café que será feito
-    }
-
-    @Override
-    public void desligar() {
-        //desliga a maquina de café
     }
 
     @Override
     public void fazerCafe() {
         //fazendo cafe
+        if (this.isLigada()) {
+            if (this.possuiCachimbo) {
+                if (this.modoCafeSelecionado != null) {
+                    //pode fazer o café
+                    this.injetarAguaCaldeira(300);
+                    this.ligarResistencia();
+                    System.out.println("Fazendo " +
+                            this.modoCafeSelecionado.getDescricao() +
+                            "..." +
+                            "\nCafé Pronto!"
+                    );
+                    this.esvaziarCaldeira();
+                    this.desligarResistencia();
+
+                } else {
+                    System.out.println("Modo de café não selecionado. Tente novamente.");
+                }
+            } else {
+                System.out.println("Dolce Gusto está sem o cachimbo. " +
+                        "Por favor insira o cachimbo para fazer o café.");
+            }
+        } else {
+            System.out.println("CAFETEIRA DESLIGADA");
+        }
     }
 
-    private void injetarAguaCaldeira() {
-        System.out.println("*** INJETANDO ÁGUA NA CALDEIRA...");
+    private boolean isLigada() {
+        return this.ligada;
+    }
+
+    private void injetarAguaCaldeira(int qtdAguaCaldeira) {
+        System.out.println("### INJETANDO ÁGUA NA CALDEIRA... " + qtdAguaCaldeira + "ML ###");
+        this.qtdAguaCaldeira = qtdAguaCaldeira;
+        System.out.println("...");
+        System.out.println("# PRONTO! #");
     }
 
     private void esvaziarCaldeira() {
-        System.out.println("*** ESVAZIANDO A CALDEIRA...");
+        System.out.println("*** ESVAZIANDO " + qtdAguaCaldeira + "ML DE ÁGUA DA CALDEIRA... ***");
+        this.qtdAguaCaldeira = 0;
+        System.out.println("CALDEIRA VAZIA");
     }
 
     private void ligarResistencia() {
-        this.ligada = true;
-        System.out.println("*** LIGANDO A RESISTÊNCIA");
+        this.resistenciaLigada = true;
+        System.out.println("*** LIGANDO A RESISTÊNCIA ***");
     }
 
     private void desligarResistencia() {
-        this.ligada = false;
-        System.out.println("*** DESLIGANDO A RESISTÊNCIA");
+        this.resistenciaLigada = false;
+        System.out.println("*** DESLIGANDO A RESISTÊNCIA ***\n");
     }
 }
---
+++ POO/abstracaoEncapsulamento/cafeteria/Main.java
@@ -0,0 +1,15 @@
+package POO.abstracaoEncapsulamento.cafeteria;
+
+public class Main {
+    public static void main(String[] args) {
+        DolceGusto maqCafe = new DolceGusto();
+
+        maqCafe.ligar();
+        maqCafe.colocarCachimbo();
+        maqCafe.selecionarModo(ModoCafe.AMERICANO);
+
+        maqCafe.fazerCafe();
+
+    }
+
+}
